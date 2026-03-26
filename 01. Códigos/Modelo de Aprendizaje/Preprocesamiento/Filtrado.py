6749b8f118cdfaec549a418b8ce08156bb494e70
Leito_uwu <leonardo.vargas.c@ucb.edu.bo>
2026-03-26 08:25:25 -0400

    cambio de filtrado

--- 01. Códigos/Modelo de Aprendizaje/Preprocesamiento/Filtrado.py
+++ 01. Códigos/Modelo de Aprendizaje/Preprocesamiento/Filtrado.py
@@ -5,14 +5,11 @@ def procesar_imagen(imagen_recortada,tamano_final = (224,224)):
     if imagen_recortada is None:
         return None
     
-    # 1. Convertir a escalas de grises
-    imagen_procesada = cv2.cvtColor(imagen_recortada,cv2.COLOR_BGR2GRAY)
-
-    # 2. Filtro Gausseano
-    # Un kernel de (5,5) es perfecto para no perder la forma del cuello
+    # Filtro Gausseano
+    # Un kernel de (5,5) 
     imagen_procesada = cv2.GaussianBlur(imagen_procesada,(5,5),0)
 
-    # 3. Redimensionamiento de la Imagen
+    # Redimensionamiento de la Imagen
     imagen_final = cv2.resize(imagen_procesada, tamano_final, interpolation=cv2.INTER_AREA)
     
     return imagen_final
